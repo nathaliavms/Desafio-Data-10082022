bb24c4345bed8e97511016fc1baf87e9eadaf62e
Nathalia Silva <Silva.Nathalia@ibm.com>
2022-08-10 16:04:03 -0300

    first commit

---
+++ Desafio10082022/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Desafio10082022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número:");
+            int a = int.Parse(Console.ReadLine());
+            ImprimaTabuada(a);
+        }
+
+        static void ImprimaTabuada(int a)
+        {
+            
+            for (int n = 1; n <= 10; ++n )
+            {
+                Console.WriteLine(n + " x " + a + " = " + n * a);
+                
+            }
+        }
+    }
+}
